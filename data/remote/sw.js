cdbd19555b045cfa23abd5c2a8298992f6d48c66
BlueLink Builder <builder@bluelink.local>
2026-06-28 01:57:19 +0200

    Update BlueLink remote viewer files

--- data/remote/sw.js
+++ data/remote/sw.js
@@ -1,7 +1,7 @@
 // BlueLink Remote PWA Service Worker
 // Cache shell assets for offline home-screen launch.
 
-const CACHE_NAME = 'bluelink-remote-v2';
+const CACHE_NAME = 'bluelink-remote-v3';
 
 const PRECACHE_URLS = [
   './bluelinkremote.html',
